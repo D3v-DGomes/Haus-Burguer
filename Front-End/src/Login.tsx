2c061232e2d644b5c61468f1a33beac0b863ddc3
D3v-DGomes <devgomes1223@hotmail.com>
2026-01-21 07:11:31 -0300

    Criando um useState para a tela de login

--- Front-End/src/Login.tsx
+++ Front-End/src/Login.tsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import Input from "./components/Input";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  }
+
   return (
-    <div className="flex h-screen items-center justify-center bg-[#161410]">
+    <form
+      className="flex h-screen items-center justify-center bg-[#161410]"
+      onSubmit={handleSubmit}
+    >
       <div className="flex flex-col justify-center gap-2">
         <img src="./logo.png" alt="" className="mb-4" />
-        <Input placeholder="E-mail" />
-        <Input placeholder="Senha" />
-        <button className="cursor-pointer rounded-md bg-[#C92A0E] py-2 text-sm font-bold text-white">
+        <Input
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className="text-white">{email}</p>
+        <button
+          className="cursor-pointer rounded-md bg-[#C92A0E] py-2 text-sm font-bold text-white"
+          onClick={handleSubmit}
+        >
           Login
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 

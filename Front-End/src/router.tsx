1e0c7578b17087937e7e3be0a72d5437ce01ba58
D3v-DGomes <devgomes1223@hotmail.com>
2026-01-22 06:37:04 -0300

    Crian Layout do projeto

--- Front-End/src/router.tsx
+++ Front-End/src/router.tsx
@@ -1,13 +1,32 @@
-import { createBrowserRouter, Navigate } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
 export const rota = createBrowserRouter([
   {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+
+  {
     path: "/",
     element: <Home />,
-    // element: <Navigate to="/login" replace />,
   },
   {
     path: "/login",

3bd50375e741e743fb8f82164a217a770a42c70c
D3v-DGomes <devgomes1223@hotmail.com>
2026-01-25 20:08:06 -0300

    Back-end para interface de cadastro

--- Back-End/index.ts
+++ Back-End/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import { connection, prisma } from "./src/db.js";
 import cors from "cors";
 
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 connection();
 
-app.post("/login", async (req, res) => {
+app.post("/login", async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
@@ -31,6 +31,36 @@ app.post("/login", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Erro no servidor." });
     return;
+  }
+});
+
+app.post("/register", async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, cep } = req.body;
+
+    if (!name || !email || !password || !cep) {
+      res
+        .status(400)
+        .json({ message: "Todas as informações são obrigatórias" });
+      return;
+    }
+
+    const user = await prisma.user.findFirst({
+      where: { email: email },
+    });
+
+    if (user?.email) {
+      res.status(409).json({ message: "E-mail já cadastrado" });
+      return;
+    }
+
+    const newUser = await prisma.user.create({
+      data: { name: name, email: email, password: password, cep: cep },
+    });
+
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: "Erro no servidor" });
   }
 });
 

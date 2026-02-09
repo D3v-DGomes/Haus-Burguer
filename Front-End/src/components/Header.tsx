bcd16406bc8bac6a9946a68651fc2781f7ad4872
D3v-DGomes <devgomes1223@hotmail.com>
2026-02-09 20:31:55 -0300

    Integração de dados do usuário via React Context

--- Front-End/src/components/Header.tsx
+++ Front-End/src/components/Header.tsx
@@ -1,24 +1,49 @@
 import { Link } from "react-router";
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
+import { LogOut, ShoppingCart, Box, LayoutDashboard, Plus } from "lucide-react";
 
 const Header = () => {
   const { user } = useContext(UserContext);
 
   return (
     <div className="bg-[#161410]">
-      <div className="mx-auto flex w-full items-center justify-between p-3 md:w-[737px] md:p-0">
+      <div className="mx-auto flex w-full items-center justify-between p-3 md:w-184.25 md:p-0">
         <img src="./logo.png" alt="" className="p-2" />
 
-        <Link to="/login">
-          {user ? (
-            <p className="text-white">Olá, {user.name}</p>
-          ) : (
-            <div className="flex h-[35px] w-[130px] cursor-pointer items-center justify-center rounded-sm bg-[#F2DAAC] font-semibold">
+        {user ? (
+          <p className="flex items-center gap-8 text-white">
+            <div className="flex items-center gap-2 text-[#F2DAAC]">
+              <div className="flex h-[32px] w-[32px] cursor-pointer items-center justify-center rounded-md border-1">
+                <Box size={22} />
+              </div>
+              <div className="flex h-[32px] w-[32px] cursor-pointer items-center justify-center rounded-md border-1">
+                <LayoutDashboard size={22} />
+              </div>
+              <div className="flex h-[32px] w-[32px] cursor-pointer items-center justify-center rounded-md border-1">
+                <Plus size={22} />
+              </div>
+            </div>
+
+            <div className="relative cursor-pointer">
+              <ShoppingCart size={22} />
+              <p className="absolute -top-4 -right-4 flex h-5 w-5 items-center justify-center rounded-full bg-[#F2DAAC] p-1 text-[#161410]">
+                1
+              </p>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <p>Olá, {user.name.split(" ")[0]}</p>
+              <LogOut size={22} className="cursor-pointer" />
+            </div>
+          </p>
+        ) : (
+          <Link to="/login">
+            <div className="flex h-8.75 w-32.5 cursor-pointer items-center justify-center rounded-sm bg-[#F2DAAC] font-semibold">
               Entrar
             </div>
-          )}
-        </Link>
+          </Link>
+        )}
       </div>
     </div>
   );
